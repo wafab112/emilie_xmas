0c80028d7b8c767902aa3121bfb243446a1339ef
wafab112 <wafab112@gmail.com>
2021-12-15 20:27:12 +0100

    cp of dev->www fixed

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cp -r /etc/letsencrypt/live/xmas-emilie.de/* /opt/emilie_xmas/backend/letsencryp
 cp -r /etc/letsencrypt/archive/xmas-emilie.de/* /opt/emilie_xmas/backend/letsencrypt/archive/xmas-emilie.de/
 
 # ToDo
-cp -r /opt/emilie_xmas/front/dev/* /opt/emilie_xmas/front/www
+cp -r -a /opt/emilie_xmas/front/dev/. /opt/emilie_xmas/front/www
 
 docker-compose up -d
