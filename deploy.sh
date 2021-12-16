9285f217de275df9dda1540501d20b8c420e7cbe
wafab112 <wafab112@gmail.com>
2021-12-16 15:26:10 +0100

    debug

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+# create db container
+docker-compose up -d
+
 mkdir -p /opt/emilie_xmas/front/letsencrypt/live/xmas-emilie.de
 mkdir -p /opt/emilie_xmas/front/letsencrypt/archive/xmas-emilie.de
 cp -r /etc/letsencrypt/live/xmas-emilie.de/* /opt/emilie_xmas/front/letsencrypt/live/xmas-emilie.de/
@@ -11,4 +14,7 @@ cp -r /etc/letsencrypt/archive/xmas-emilie.de/* /opt/emilie_xmas/backend/letsenc
 # ToDo
 cp -r -a /opt/emilie_xmas/front/dev/. /opt/emilie_xmas/front/www
 
-docker-compose up -d
+cd backend/api; dotnet ef database update
+
+cd backend; docker-compose up -d
+cd front; docker-compose up -d
